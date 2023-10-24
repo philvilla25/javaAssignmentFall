863605c38ed7941031bdecb11908b60663aef6f3
Daniella Akpoguma <rukevweakpoguma11@gmail.com>
2023-10-24 19:22:41 -0400

    Added new classes: Game, GameController, etc

---
+++ src/Game.java
@@ -0,0 +1,12 @@
+
+public class Game {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		GameModel gameModel = new GameModel();
+		GameView gameView = new GameView();
+	    GameController gameController = new GameController(gameModel, gameView);
+
+	}
+
+}
---
+++ src/GameController.java
@@ -0,0 +1,8 @@
+
+public class GameController {
+
+	public GameController(GameModel gameModel, GameView gameView) {
+		// TODO Auto-generated constructor stub
+	}
+
+}
